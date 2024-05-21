61a4fea0e97db452e4c8828319802a24293f9424
shahzaib-1997 <smsashafqat@gmail.com7>
2024-05-22 04:00:33 +0500

    corrected home url

--- userapi/urls.py
+++ userapi/urls.py
@@ -1,14 +1,13 @@
 # api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectAPIView, TaskAPIView, UserViewSet, home
+from .views import ProjectAPIView, TaskAPIView, UserViewSet
 
 router = DefaultRouter()
 router.register(r'auth', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', home, name='home'),
     path('project/', ProjectAPIView.as_view(), name="projects"),
     path('project/<int:pk>/', ProjectAPIView.as_view(), name="projects"),
     path('task/', TaskAPIView.as_view(), name="tasks"),
